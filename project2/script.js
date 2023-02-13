cec0a81808000ab44c52986ba6778ba51086ab60
Holdrulff <wesley.fernan1@gmail.com>
2023-02-12 23:55:09 -0300

    updating

--- project2/script.js
+++ project2/script.js
@@ -2,5 +2,5 @@ function calcular() {
   let text = document.getElementById("textarea").value;
   let result = text.split(" ").length;
 
-  document.getElementById("resultado").innerHTML = result + "palavras";
+  document.getElementById("resultado").innerHTML = result + " palavras";
 }
